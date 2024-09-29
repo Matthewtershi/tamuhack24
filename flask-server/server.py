07ba9104da1ddb70db75f373fbde7fbcb4ebfca2
Arjun Mahableshwarkar <lordwhitley@gmail.com>
2024-09-28 23:32:17 -0500

    model adding

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,4 +1,29 @@
 from flask import Flask
+import sqlite3
+
+conn = sqlite3.connect("testerdb.db")
+
+cursor = conn.cursor()
+
+cursor.execute("""CREATE TABLE users (
+                email text,
+                first text,
+                last text,
+                user_ID integer
+                )""")
+
+cursor.execute("""CREATE TABLE transactions (
+                email text,
+                restaurant text,
+                item text,
+                purchase_amount integer,
+                date text
+               
+                )""")
+
+conn.commit()
+
+conn.close()
 
 app = Flask(__name__)
 
@@ -6,7 +31,7 @@ app = Flask(__name__)
 @app.route("/")
 def home_page():
 
-    
+
     return 
 
 if __name__ == "__main__":
