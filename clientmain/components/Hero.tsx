340524fe8f036cfca50b484763d76f112cb2f092
Matthewtershi <matthewtershi@gmail.com>
2024-09-28 23:36:39 -0500

    temp

--- clientmain/components/Hero.tsx
+++ clientmain/components/Hero.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { GlobeDemo } from "./ui/gridglobe";
 import temp from "../../client/public/head2.png";
 import frame from "../../client/public/frame2.png";
+import "animate.css"
 import yellow from "../../client/public/yellowflame.png";
 import orange from "../../client/public/orangeflame.png";
 import red from "../../client/public/redflame.png";
@@ -11,19 +12,19 @@ import "../../client/public/general.css";
 const Hero = () => {
     return (
         <div className="w-full mx-auto h-[88vh] flex text-black">
-            <div className="gap-x-10 mx-auto flex py-20 left-10">
+            <div className="gap-x-10 mx-auto flex py-20 left-10" style={{userSelect: "none"}}>
                 <div> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </div>
                 <div className="flex-col flex mb-10 p-4 justify-center items-center">
                     <Image src={temp} alt={""} className="w-23"/> 
-                    <button className="ihatevegans btn-34"> <span> Learn More! </span> </button>
+                    <button className="animate__slideInLeft ihatevegans btn-34"> <span> Learn More! </span> </button>
                 </div>
 
                 <div className="flex flex-col relative right-0 top-[-25vh] w-[600px] h-[600px]">
                     <GlobeDemo />
                     <div className="" style= {{
-                        transform: "translateX(65.77px) translateY(300px)",
-                        width: "900px",
-                        height: "auto",
+                        transform: "translateX(-435px) translateY(115px)",
+                        width: "1100px",
+                        height: "1100px",
                     }}
                     >
 
