cfec4a1299157501f0a0a31a19de2081b94fc7dd
Matthewtershi <matthewtershi@gmail.com>
2024-09-29 01:25:32 -0500

    bug fixes

--- clientmain/app/page.tsx
+++ clientmain/app/page.tsx
@@ -7,7 +7,7 @@ import { FloatingNav } from "@/components/ui/floatingNav";
 export default function Home() {
 
   return (
-    <div className="bg-beige justify center items-center">
+    <div className="bg-beige justify center items-center overflow-hidden">
       <FloatingNav navItems={[{ name: "Home", link: "#home" }, { name: "What We Do", link: "#map" },{ name: "Our Process", link: "#process"},]}/>
       <div className="bg-darkbrown h-[6vh] w-full"/>
       <div className="bg-lightbrown h-[6vh] w-full"/>
--- clientmain/components/Hero.tsx
+++ clientmain/components/Hero.tsx
@@ -18,10 +18,26 @@ const Hero = () => {
                 }}>
                 <div> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </div>
                 <div className="flex-col flex mb-10 p-4 justify-center items-center">
+                    <div className="flex gap-x-10">
+                        <Image src={yellow} alt={""} style={{
+                            width:"100px",
+                            height:"100px",
+                        }}/> 
+                        <Image src={orange} alt={""} style={{
+                            width:"100px",
+                            height:"100px",
+                        }}/> 
+                        <Image src={red} alt={""} style={{
+                            width:"100px",
+                            height:"100px",
+                        }}/> 
+                    </div>
                     <Image src={temp} alt={""} className="w-21"/> 
                     <button className="animate_animated animate_slideInLeft btn-34"> <span> Learn More! </span> </button>
                 </div>
-                <div className="flex flex-col relative right-50px top-[-25vh] w-[600px] h-[600px]">
+                <div className="flex flex-col relative right-50px top-[-25vh] w-[600px] h-[600px]" style={{
+                    transform: "translateY(50px)",
+                }}>
                     <GlobeDemo />
                     <div style= {{
                         transform: "translateX(-435px) translateY(115px)",
